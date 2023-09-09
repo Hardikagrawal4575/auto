50fd89a9c541296a7edee7089e0ca134fcf1fdf5
hardik agrawal <hardik.agra12345@gmail.com>
2023-09-09 17:50:04 +0530

    files added

---
+++ chatBot.py
@@ -0,0 +1,50 @@
+import os
+import time
+import asyncio
+import threading
+from telethon.sync import TelegramClient
+from colorama import Fore, Back, Style
+from telethon import functions, types
+from SendReaction import run_SendReaction
+from views import view_main
+
+threads=[]
+async def getClient(phone):
+    try:
+        print(f'Authenticating phone number: {phone}')
+        client = TelegramClient(f'sessions/{phone}', 2392599, '7e14b38d250953c8c1e94fd7b2d63550')
+        await client.start()
+
+        if not await client.is_user_authorized():
+            await client.send_code_request(phone)
+            await client.sign_in(phone, input(f'Phone # ({phone}) Enter the code: '))
+        return client
+    except Exception as e:
+        print(f'Error: {str(e)}')
+        return None
+
+async def getMessagesID(client):
+    try:
+        async for message in client.iter_messages('ydhsbsbad'):
+            print(f'Message from {message.sender_id}: {message.text} : link https://t.me/ydhsbsbad/{message.id}')
+            return message.id,message.text
+    except Exception as e:
+        print(f'Error while getting messages: {str(e)}')
+
+async def startBot():
+    phone = '+918971008788'
+    chat_id = 'ydhsbsbad'
+    client = await getClient(phone)
+    lastMessage = None
+    
+    if client is not None:
+        print('Bot started.')
+        while True:
+            msg_id, text = await getMessagesID(client)
+            if lastMessage == None or lastMessage != msg_id:
+                task = asyncio.create_task(run_SendReaction( chat_id,msg_id))  # Run in the event loop concurrently
+                task2 = asyncio.create_task(view_main(chat_id,msg_id))
+                lastMessage = msg_id
+            await asyncio.sleep(10)
+
+asyncio.run(startBot())
